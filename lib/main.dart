56085b8e24a87fca8894d229c62c2ff41ef303af
Ajay Kumar Dewangan <dewanganajay05@gmail.com>
2024-06-25 18:07:55 +0530

    _

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,8 +19,41 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
 
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+
+          onPressed: (){
+
+
+            Person person  = Person(name: 'Ajay', age: 21);
+            Person person1  = Person(name: 'Ajay', age: 21);
+
+            print(person.hashCode.toString());
+            print(person1.hashCode.toString());
+            print(person==person1);
+          },
+        ),
+
       ),
 
+
     );
+
   }
+}
+
+
+class Person extends Equatable{
+
+  final String name;
+  final int age;
+
+  // Constructor
+  const Person({required this.name, required this.age});
+
+  @override
+
+  List<Object?> get props => [name,age];
+
+
 }
